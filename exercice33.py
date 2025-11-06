667326609bcc232e25b79fb715214ccf0c451f26
leuvez <vincentbrn@hotmail.fr>
2025-11-06 17:27:31 +0100

    Update exercice33.py

--- exercice33.py
+++ exercice33.py
@@ -6,12 +6,14 @@ def simulation_lancer_de(n):
    for i in range(1, n+1):
             random.randint(1, 6)
             nombre_de_lancers += 1 
+            
    if (random.randint(1, 6) == 6):
          nombre_de_6 += 1
          freq_de_6 = nombre_de_6 / nombre_de_lancers
+         print('nombre de fois 6 :',nombre_de_6)
+         print('frequence de 6 :',freq_de_6)
          return freq_de_6
    
+   
    print('nombre de lancer:', nombre_de_lancers)
-   print('nombre de fois 6 :',nombre_de_6)
-   print('frequence de 6 :',freq_de_6)
-print(simulation_lancer_de(1000))
+print(simulation_lancer_de(10))
